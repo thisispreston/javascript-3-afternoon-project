d202d4377addf8e4dbc3fc4901016d4bac6066f7
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-01-17 22:33:03 -0700

    Yatta

--- nesting.js
+++ nesting.js
@@ -50,8 +50,16 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
-
+function employeeUpdater () {
+  for (i = 0; i < employees.length; i++) {
+    if (employees[i].firstName === 'Theo') {
+      employees.splice(i, 1)
+    } 
+    if (employees[i].firstName === 'Lorie') {
+      employees[i].department = 'HR'
+    }
+  } return employees
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -68,8 +76,15 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
-
+function removeDuplicates () {
+  for (let i = 0; i < workplaceAccidents.length; i++) {
+    for (let j = i + 1; j< workplaceAccidents.length; j++) {
+      if (workplaceAccidents[i] === workplaceAccidents[j]) {
+        workplaceAccidents.splice(i, 1)
+      }
+    }
+  } return workplaceAccidents
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -96,10 +111,9 @@ var cat = {
     2. Assign fluffy2ndFriend the name of Fluffy's 2nd friend.
 */
 
-//Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1]
 
+var fluffy2ndFriend = cat.catFriends[1].name
 
 
 ////////// PROBLEM 4 //////////
@@ -138,8 +152,12 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
-
+function recordCleaner () {
+  for (let i = 0; i < myCar.accidents.length; i++) {
+    if (myCar.accidents[i].atFaultForAccident)
+      myCar.accidents[i].atFaultForAccident = false
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -157,6 +175,17 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+looper = () => {
+  for (var i = 0; i < numsArr.length; i++) {
+    for (var j = 0; j < numsArr[i].length; j++) {
+        if (numsArr[i][j] % 2 === 0) {
+          numsArr[i][j] = 'even';
+        } else if (numsArr[i][j] % 2 === 1) {
+          numsArr[i][j] = 'odd'
+        }
+    }
+  } return numsArr
+}
 
+looper()
 
